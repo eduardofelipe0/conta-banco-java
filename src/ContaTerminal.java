e862d8fa4541aefcdca6753b918aaed2506dd087
Eduardo Felipe da Silva <eduardo.felipesilva99@gmail.com>
2026-02-28 15:27:31 -0300

    implementacao do metodo criar conta

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("\nOlá, seja bem-vindo ao Banco DIO!");
+        System.out.println("-------------------------------");
+        System.out.println("Abaixo segue as opções disponíveis para você:\n");
+        System.out.println("1 - Criar conta");
+        System.out.println("2 - Sair");
+
+        System.out.print("\nEscolha uma opção: ");
+        int opcao = scanner.nextInt();
+
+        if (opcao == 1) {
+            criarConta(scanner);
+        } else if (opcao == 2) {
+            System.out.println("\nEncerrando o programa... Obrigado por usar nossos serviços!");
+        } else {
+            System.out.println("Opção inválida. Por favor, tente novamente.");
+        }
+    }
+
+    private static void criarConta(Scanner scanner) {
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("\nVamos criar sua conta! Por favor, forneça as seguintes informações:");
+
+        System.out.print("\nNúmero da agência: ");
+        agencia = scanner.next();
+
+        System.out.print("Número da conta: ");
+        numeroConta = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.print("Nome do cliente: ");
+        nomeCliente = scanner.nextLine();
+        
+        System.out.print("Saldo inicial separado por vírgula: R$ ");
+        saldo = scanner.nextDouble();
+
+        System.out.printf("\nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo é de R$ %.2f",
+                nomeCliente, agencia, numeroConta, saldo);
     }
 }
